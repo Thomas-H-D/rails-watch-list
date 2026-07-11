6d73880b3616366e113f1ccd1885c3b39baffc95
Thomas Davidsen <260642635+Thomas-H-D@users.noreply.github.com>
2026-07-11 16:53:07 +0900

    cleared all rspec stages of the models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :destroy
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+  has_many :bookmarks
   has_many :lists, through: :bookmarks
 end
