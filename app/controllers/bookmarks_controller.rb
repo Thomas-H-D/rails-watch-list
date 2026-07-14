a389a657f7d7e92ebc5d6449fe17f05d3ea7c2e5
Thomas Davidsen <260642635+Thomas-H-D@users.noreply.github.com>
2026-07-14 21:00:00 +0900

    can remove bookmarks and also added some basic styling

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -20,6 +20,13 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
+  end
+
   private
 
   def bookmark_params
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [ :new, :create ]
   end
+    resources :bookmarks, only: [:destroy]
 end
